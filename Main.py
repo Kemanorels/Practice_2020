2f8b539dc2004998384b0b903ec22016819b2627
Kemanorel <lord.kemanorel@gmail.com>
2020-08-08 13:09:47 +0300

    'Version3'

--- Main.py
+++ Main.py
@@ -1,21 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 URL = 'https://autosila-amz.com/zapchasti-dlya-sng/vaz/2101/dvigatel/dvigatel/' #Сайт который парсим
 # URL = 'https://autosila-amz.com/zapchasti-dlya-inomarok/daewoo/'
-
+FILE = 'All_product.csv'
 
 def get_html(url, params = None):
     req = requests.get(url, params=params)
     return req
 
+
 def get_pages(html):
+    '''
+    Служит для остановки парсинга, проверяет содержимое страницы'''
     soup = BeautifulSoup(html, 'html.parser')
     stop = soup.find('span', class_='ty-pagination__selected')
     return stop
 
-get_pages(get_html(URL).text)
+
+def save_file(items, path):
+        with open(path, 'w', newline='') as file:
+            writer = csv.writer(file, delimiter = ';')
+            writer.writerow(['name', 'price', 'href'])
+            for item in items:
+                writer.writerow([item['name'], item['price'], item['href']])
 
 
 def get_content(html):
@@ -24,9 +34,13 @@ def get_content(html):
     products = []
     for item in items:
         if item.find('span', class_='ty-price-num') != None:
+            '''
+            проверяет наличие цены (есть позиции без цен)
+            добавляет в список
+            '''
             products.append({
                 'name' : item.find('a', class_='product-title').get_text(),
-                'price' : ''.join(item.find('span', class_='ty-price-num').text.split()),
+                'price' : ''.join(item.find('span', class_='ty-price-num').text.split(',')),
                 'href' : item.find('a', 'product-title').get('href'),
                 })
         else:
@@ -38,15 +52,18 @@ def get_content(html):
     return products
 
 
-
 def main():
-    page = 0
-    item = []
+    page = 1
+    items = []
     while get_pages(get_html(URL, params={'page': page}).text) != None:
-        item.append(get_content(get_html(URL, params={'page': page}).text))
+        '''
+        Пока в содержимом страницы есть get_pages, добавляет параметры и парсит страницу
+        как только страницы закончатся останавливается
+        '''
+        items.extend(get_content(get_html(URL, params={'page': page}).text))
         page += 1
-        print(f'Парсим страницу{page}')
-    print(len(item))
+        print(f'Парсим страницу {page-1}')
+    save_file(items, FILE)
 
 
 if __name__ == '__main__':
